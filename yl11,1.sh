ac0373864d1e0f804f000c04b8dc44de999dcc79
Karl Jugapuu <98745340+KarlJug@users.noreply.github.com>
2023-10-11 12:13:28 +0300

    Delete yl11,1.sh

--- yl11,1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Sisesta program: " prog
-
-if systemctl is-active "$prog" > == /dev/null; then
-	echo " $prog on aktiivne ja töötab."
-else
-	echo " Hoiatus: $prog ei ole aktiivne!"
-fi
