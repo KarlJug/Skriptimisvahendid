c58215128bac85412a109c4f3b9b186de179c764
Karl Jugapuu <98745340+KarlJug@users.noreply.github.com>
2023-10-31 11:50:05 +0200

    Add files via upload

---
+++ JS/Ülesanne 15/ylesanne15.js
@@ -0,0 +1,11 @@
+/**
+ * Karl Jugapuu
+ * Üleasnne 15
+ * 31.10.2023
+ */
+
+
+let theme = document.getElementById("theme");
+theme.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
